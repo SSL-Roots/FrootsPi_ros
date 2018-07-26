d4a2be0e6138b7f1ed50dab727ab1360ccf88093
ShotaAk <macakasit@gmail.com>
2018-07-26 14:57:00 +0000

    Add pigpio installation to travis_build_frootspi.bash

--- test_scripts/travis_build_frootspi.bash
+++ test_scripts/travis_build_frootspi.bash
@@ -3,6 +3,13 @@
 # Install packages
 sudo apt -y install ros-indigo-joy
 
+# Install pigpio
+wget abyz.me.uk/rpi/pigpio/pigpio.zip
+unzip pigpio.zip
+cd PIGPIO
+make
+sudo make install
+
 # Build FrootsPi
 rsync -av ./ ~/catkin_ws/src/FrootsPi
 
