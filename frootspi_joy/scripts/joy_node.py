b097f81f4363398ee60213ba324184470747af99
ykek <y.kek.na0710@gmail.com>
2018-07-28 23:34:31 -0800

    add input joystick

--- frootspi_joy/scripts/joy_node.py
+++ frootspi_joy/scripts/joy_node.py
@@ -4,7 +4,12 @@
 import rospy
 from sensor_msgs.msg import Joy
 from frootspi_msg.msg import FrootsCommand
-
+from math import atan2 
+from math import degrees 
+from math import radians 
+from math import pow 
+from math import sqrt
+from math import pi
 
 class Core(object):
     def __init__(self):
@@ -21,11 +26,18 @@ class Core(object):
         self._SEL = rospy.get_param('~button_SEL')
         self._START = rospy.get_param('~button_START')
 
+        self._Lside_axis_X = rospy.get_param('~Lside_axis_X')
+        self._Lside_axis_Y = rospy.get_param('~Lside_axis_Y')
+        self._Rside_axis_X = rospy.get_param('~Rside_axis_X')
+        self._Rside_axis_Y = rospy.get_param('~Rside_axis_Y')
+
         self._dribble_power = 6
         self._dribble_MAX = 15
 
         self._kick_power = 6
         self._kick_MAX = 15
+        
+        self._velocity_max = 32767
 
         self._command = FrootsCommand()
 
@@ -64,8 +76,31 @@ class Core(object):
             command.kick_flag = False
             command.kick_power = 0
 
+        # Velocity convert
+        self.vel_norm  = sqrt(pow(msg.axes[self._Lside_axis_X], 2) + pow(msg.axes[self._Lside_axis_Y], 2))
+        try:
+            self.vel_theta = atan2(msg.axes[self._Lside_axis_Y], -msg.axes[self._Lside_axis_X]) 
+        except ZeroDivisionError:
+            if msg.axes[self._Lside_axis_X] > 0:
+                self.vel_theta = pi / 2
+            elif msg.axes[self._Lside_axis_X] < 0:
+                self.vel_theta = -pi / 2
+            else :
+                self.vel_theta = 0
+
+        if msg.buttons[self._L]:
+            self.vel_omega = -0.5
+        elif msg.buttons[self._R]:
+            self.vel_omega = 0.5
+        else:
+            self.vel_omega = 0.0
+        
+        command.vel_norm  = self.vel_norm 
+        command.vel_theta = self.vel_theta / pi * 180
+        command.vel_omega = self.vel_omega
         self._command = command
 
+         
 
     def _dribble_power_control(self, joy_msg, dribble_power):
         if joy_msg.buttons[self._X]:
