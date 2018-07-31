c1c587a1a12238059e7ec4f05cc3f7e1da1c28e3
ShotaAk <macakasit@gmail.com>
2018-07-31 14:31:13 +0000

    Fix travis_ros_install

--- test_scripts/travis_ros_install.bash
+++ test_scripts/travis_ros_install.bash
@@ -3,13 +3,13 @@
 # Install ROS
 # Reference : http://wiki.ros.org/ja/indigo/Installation/Ubuntu
 
-# sudo sh -c 'echo "deb http://packages.ros.org/ros/ubuntu trusty main" > /etc/apt/sources.list.d/ros-latest.list'
-sudo sh -c 'echo "deb http://packages.ros.org/ros/ubuntu $(lsb_release -sc) main" > /etc/apt/sources.list.d/ros-latest.list'
+sudo sh -c 'echo "deb http://packages.ros.org/ros/ubuntu trusty main" > /etc/apt/sources.list.d/ros-latest.list'
 
-#wget http://packages.ros.org/ros.key -O - | sudo apt-key add -
-sudo apt-key adv --keyserver hkp://ha.pool.sks-keyservers.net:80 --recv-key 421C365BD9FF1F717815A3895523BAEEB01FA116
+wget http://packages.ros.org/ros.key -O - | sudo apt-key add -
 
 sudo apt-get update 
+
+sudo apt-get install -y python-rosdep python-catkin-tools
 
 sudo apt-get -y install ros-indigo-ros-base
 
